d62acdc956a7727f25412fdc59f13a94d9b321e5
Daniel Olsen <daniel.olsen99@gmail.com>
2021-09-06 10:34:39 +0200

    static dav1d tests

--- default.nix
+++ default.nix
@@ -3,9 +3,14 @@ let
 in
 rec {
     libaom = pkgs.callPackage ./pkgs/libaom {};
+    dav1d = pkgs.callPackage ./pkgs/dav1d {};
+
     visqol = pkgs.callPackage ./pkgs/visqol {};
     bopus = pkgs.callPackage ./pkgs/bopus { visqol = visqol; };
-    ffmpeg = pkgs.ffmpeg.override { libaomSupport = true; libaom = pkgs.libaom; };
-    ffmpeg_static_dav1d = pkgs.ffmpeg-full.override { dav1d = pkgs.pkgsStatic.dav1d; };
-    mpv_static_dav1d = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { ffmpeg = ffmpeg_static_dav1d; }) {};
+
+#    ffmpeg = pkgs.ffmpeg-full.override { libaom = pkgs.libaom; };
+
+
+    ffmpeg-static-dav1d = pkgs.ffmpeg-full.override { dav1d = pkgs.pkgsStatic.dav1d; };
+    mpv-static-dav1d = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { ffmpeg = ffmpeg-static-dav1d; }) {};
 }
