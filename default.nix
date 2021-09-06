58dabb69a47a4ae4a0206cbc55648e474d5e234c
Daniel Olsen <daniel.olsen99@gmail.com>
2021-09-06 09:48:17 +0200

    mpv static dav1d

--- default.nix
+++ default.nix
@@ -5,5 +5,7 @@ rec {
     libaom = pkgs.callPackage ./pkgs/libaom {};
     visqol = pkgs.callPackage ./pkgs/visqol {};
     bopus = pkgs.callPackage ./pkgs/bopus { visqol = visqol; };
-    ffmpeg = pkgs.ffmpeg.override { libaomSupport = true; libaom = libaom; };
+    ffmpeg = pkgs.ffmpeg.override { libaomSupport = true; libaom = pkgs.libaom; };
+    ffmpeg_static_dav1d = pkgs.ffmpeg-full.override { dav1d = pkgs.pkgsStatic.dav1d; };
+    mpv_static_dav1d = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { ffmpeg = ffmpeg_static_dav1d; }) {};
 }
