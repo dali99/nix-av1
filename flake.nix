5312bf0d264d22421919d5d28de25574abf134f1
Daniel Olsen <daniel.olsen99@gmail.com>
2022-05-11 00:16:22 +0200

    Update aomen-psy

--- flake.nix
+++ flake.nix
@@ -7,14 +7,14 @@
 
     packages.x86_64-linux.aom-av1-psy =
       with import nixpkgs { system = "x86_64-linux"; };
-      (libaom.override {enableButteraugli = true; enableVmaf = true;}).overrideAttrs (old: {
+      libaom.overrideAttrs (old: {
         pname = "aom-av1-psy";
-        version = "0.1.2";
+        version = "0.1.4";
         src = fetchFromGitHub {
           owner = "BlueSwordM";
           repo = "aom-av1-psy";
-          rev = "full-aom-av1-psy-0.1.2";
-          sha256 = "15767p33wpr6w7w7avi7rlf4b71947mczgqcsbakrhjhcfybzs61";
+          rev = "full-aom-av1-psy-0.1.4";
+          sha256 = "18agipmmyzkc6dj58x75w948k88364d734d1wbl60zz77gcrcd61";
         };
       });
 
