f415341de881dae117820753eddbb37f98b6c4ea
Daniel Olsen <daniel.olsen99@gmail.com>
2022-05-12 06:20:58 +0200

    flake: support more systems

--- default.nix
+++ default.nix
@@ -4,26 +4,12 @@ in
 rec {
     libaom = pkgs.callPackage ./pkgs/libaom {};
 
-    aom-av1-psy = (pkgs.libaom
-      .override { enableButteraugli = true; enableVmaf = true;})
-      .overrideAttrs (old: {
-        pname = "aom-av1-psy";
-        version = "0.1.2";
-        src = pkgs.fetchFromGitHub {
-          owner = "BlueSwordM";
-          repo = "aom-av1-psy";
-          rev = "full-aom-av1-psy-0.1.2";
-          sha256 = "15767p33wpr6w7w7avi7rlf4b71947mczgqcsbakrhjhcfybzs61";
-        };
-      });
-
     dav1d = pkgs.callPackage ./pkgs/dav1d {};
 
     visqol = pkgs.callPackage ./pkgs/visqol {};
     bopus = pkgs.callPackage ./pkgs/bopus { visqol = visqol; };
 
 #    ffmpeg = pkgs.ffmpeg-full.override { libaom = pkgs.libaom; };
-
 
     ffmpeg-static-dav1d = pkgs.ffmpeg-full.override { dav1d = pkgs.pkgsStatic.dav1d; };
     mpv-static-dav1d = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { ffmpeg = ffmpeg-static-dav1d; }) {};
--- flake.nix
+++ flake.nix
@@ -1,26 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "Flake providing aom-av1-psy";
 
   inputs.nixpkgs.url = github:Dali99/nixpkgs/libaom-tunes;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.aom-av1-psy = pkgs.libaom.overrideAttrs (old: {
+          pname = "aom-av1-psy";
+          version = "0.1.4";
+          src = pkgs.fetchFromGitHub {
+            owner = "BlueSwordM";
+            repo = "aom-av1-psy";
+            rev = "full-aom-av1-psy-0.1.4";
+            sha256 = "18agipmmyzkc6dj58x75w948k88364d734d1wbl60zz77gcrcd61";
+          };
+        });
 
-    packages.x86_64-linux.aom-av1-psy =
-      with import nixpkgs { system = "x86_64-linux"; };
-      libaom.overrideAttrs (old: {
-        pname = "aom-av1-psy";
-        version = "0.1.4";
-        src = fetchFromGitHub {
-          owner = "BlueSwordM";
-          repo = "aom-av1-psy";
-          rev = "full-aom-av1-psy-0.1.4";
-          sha256 = "18agipmmyzkc6dj58x75w948k88364d734d1wbl60zz77gcrcd61";
-        };
-      });
-
-    apps.x86_64-linux.aomenc = {
-      type = "app";
-      program = "${self.packages.x86_64-linux.aom-av1-psy.bin}/bin/aomenc";
-    };
-  };
+        apps.aomenc = {
+          type = "app";
+          program = "${self.packages.${system}.aom-av1-psy.bin}/bin/aomenc";
+        };    
+      }
+    );
 }
