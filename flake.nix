7803fc9821a1759f6e46ad7189bc30dcd8d971c4
Daniel Olsen <daniel.olsen99@gmail.com>
2022-06-04 05:48:29 +0200

    update aom-psy

--- flake.nix
+++ flake.nix
@@ -10,12 +10,12 @@
       in {
         packages.aom-av1-psy = pkgs.libaom.overrideAttrs (old: {
           pname = "aom-av1-psy";
-          version = "unstable-2022-05-27";
+          version = "unstable-2022-06-03";
           src = pkgs.fetchFromGitHub {
             owner = "BlueSwordM";
             repo = "aom-av1-psy";
-            rev = "2f86189644bcd3fcf37c8ec19bbf9394d9de7fc1";
-            sha256 = "sha256-tjlYvma30Ht2m0IX2RGw1Mu8nXcd7RLhmP0CNfadJp0=";
+            rev = "6fa36a27755a2d39dca9626a7b4e73bab7a374ad";
+            sha256 = "sha256-/FwdYqLzund/uHtOfyu2DhsqK09YWUifjV9sBOevDK4=";
           };
         });
 
