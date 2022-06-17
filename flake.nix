46b49c36b7a607607ffe9ea589e2d60b6387913e
Daniel Olsen <daniel.olsen99@gmail.com>
2022-06-18 01:22:59 +0200

    statically linked aom

--- flake.nix
+++ flake.nix
@@ -1,22 +1,29 @@
 {
   description = "Flake providing aom-av1-psy";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/826794479e937f70ee2dad3d5cac38d3a0a5cb45;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/397724176b474080a09422fdc29d3fc85cd43f2a";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.aom-av1-psy.url = "github:/BlueSwordM/aom-av1-psy/6fa36a27755a2d39dca9626a7b4e73bab7a374ad";
   inputs.aom-av1-psy.flake = false;
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  let
+    aom-av1-psy = pkgs: (pkgs.libaom.overrideAttrs (old: {
+      pname = "aom-av1-psy";
+      version = "unstable";
+      src = inputs.aom-av1-psy;
+    })).override { enableButteraugli = true; };
+  in
+    {
+      overlay = final: prev: {
+        libaom-psy = aom-av1-psy prev;
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { overlays = [ self.overlay ]; config = { inherit system; }; };
       in {
-        packages.aom-av1-psy = (pkgs.libaom.overrideAttrs (old: {
-          pname = "aom-av1-psy";
-          version = "unstable";
-          src = inputs.aom-av1-psy;
-        })).override { enableButteraugli = true; };
-
+        packages.aom-av1-psy = pkgs.libaom-psy;
         apps.aomenc-psy = {
           type = "app";
           program = "${self.packages.${system}.aom-av1-psy.bin}/bin/aomenc";
@@ -24,13 +31,18 @@
 
         hydraJobs.aom-av1-psy = self.packages.${system}.aom-av1-psy;
         
-        
-        
+                
         packages.visqol = pkgs.callPackage ./pkgs/visqol { };
         packages.bopus = pkgs.callPackage ./pkgs/bopus { visqol = self.packages.${system}.visqol; };
         
         packages.ffmpeg-static-dav1d = pkgs.ffmpeg-full.override { dav1d = pkgs.pkgsStatic.dav1d; };
         packages.mpv-static-dav1d = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { ffmpeg = self.packages.${system}.ffmpeg-static-dav1d; }) { };
+        
+        
+        packages.libaom-psy-static = (pkgs.pkgsStatic.libaom-psy.overrideAttrs (old: {
+          cmakeFlags = [ "-DCMAKE_C_FLAGS_INITgs=-static" ];
+          postInstall = "echo empty > $out/test";
+        })).override { enableButteraugli = false; };
       }
     );
 }
