d30722d2ea43e3699ac5612d9c89ad31d0e652a6
Daniel Olsen <daniel.olsen99@gmail.com>
2022-06-17 23:36:14 +0200

    only flake

--- flake.nix
+++ flake.nix
@@ -1,30 +1,36 @@
 {
   description = "Flake providing aom-av1-psy";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/staging;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/826794479e937f70ee2dad3d5cac38d3a0a5cb45;
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }: 
+  inputs.aom-av1-psy.url = "github:/BlueSwordM/aom-av1-psy/6fa36a27755a2d39dca9626a7b4e73bab7a374ad";
+  inputs.aom-av1-psy.flake = false;
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs: 
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages.aom-av1-psy = (pkgs.libaom.overrideAttrs (old: {
           pname = "aom-av1-psy";
-          version = "unstable-2022-06-03";
-          src = pkgs.fetchFromGitHub {
-            owner = "BlueSwordM";
-            repo = "aom-av1-psy";
-            rev = "6fa36a27755a2d39dca9626a7b4e73bab7a374ad";
-            sha256 = "sha256-/FwdYqLzund/uHtOfyu2DhsqK09YWUifjV9sBOevDK4=";
-          };
+          version = "unstable";
+          src = inputs.aom-av1-psy;
         })).override { enableButteraugli = true; };
 
-        apps.aomenc = {
+        apps.aomenc-psy = {
           type = "app";
           program = "${self.packages.${system}.aom-av1-psy.bin}/bin/aomenc";
         };
 
         hydraJobs.aom-av1-psy = self.packages.${system}.aom-av1-psy;
+        
+        
+        
+        packages.visqol = pkgs.callPackage ./pkgs/visqol { };
+        packages.bopus = pkgs.callPackage ./pkgs/bopus { visqol = self.packages.${system}.visqol; };
+        
+        packages.ffmpeg-static-dav1d = pkgs.ffmpeg-full.override { dav1d = pkgs.pkgsStatic.dav1d; };
+        packages.mpv-static-dav1d = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { ffmpeg = self.packages.${system}.ffmpeg-static-dav1d; }) { };
       }
     );
 }
